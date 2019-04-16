6077e33cce693eddb45cb248130ae301b2a41ba0
Nero <nerochen25@berkeley.edu>
2019-04-16 11:54:08 -0700

    Add src folder with App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text> 
+                    App
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default App;
